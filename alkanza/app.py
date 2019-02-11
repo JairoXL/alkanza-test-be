079d2b3088be74dda12bf405e415c1b38890ff2d
Jairo Andrés Mendieta <37624594+JairoXL@users.noreply.github.com>
2019-02-11 11:19:14 -0500

    Update app.py

--- alkanza/app.py
+++ alkanza/app.py
@@ -2,8 +2,35 @@ import falcon
 from .resources import *
 from falcon_autocrud.middleware import Middleware
 
+
+class CORSComponent(object):
+    def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+
+        if (req_succeeded
+            and req.method == 'OPTIONS'
+            and req.get_header('Access-Control-Request-Method')
+        ):
+            # NOTE(kgriffs): This is a CORS preflight request. Patch the
+            #   response accordingly.
+
+            allow = resp.get_header('Allow')
+            resp.delete_header('Allow')
+
+            allow_headers = req.get_header(
+                'Access-Control-Request-Headers',
+                default='*'
+            )
+
+            resp.set_headers((
+                ('Access-Control-Allow-Methods', allow),
+                ('Access-Control-Allow-Headers', allow_headers),
+                ('Access-Control-Max-Age', '86400'),  # 24 hours
+            ))
+            
+
 api = application = falcon.API(
-    middleware=[Middleware()]
+    middleware=[Middleware(), CORSComponent()]
 )
 
 engine = create_engine('sqlite:///database.db')
